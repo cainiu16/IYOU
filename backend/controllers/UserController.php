0691911d410542214ef1756a92e978cb052f732f
zjh <15710071323@163.com>
2016-03-27 20:15:36 +0800

    collect

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -6,6 +6,8 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use common\models\LoginForm;
 use yii\filters\VerbFilter;
+use app\models\User;
+use app\models\message;
 
 /**
  * Site controller
@@ -18,6 +20,23 @@ class UserController extends Controller
     public function actionIndex()
     {
     	$this->layout='header';
-    	return $this->render('user');
+    	$model = new User;
+    	$select = $model->selects();
+    	return $this->render('user',['select'=>$select]);
+    	// print_r($select);die;
+    }
+    public function actionUser_del()
+    {
+    		$model = new User;
+    	    $models = new message;
+    		$id = $_GET['id'];
+    		// print_r($id);die;
+    		$del = $model->deleteAll("u_id = '$id'");
+    		$dels = $models->deleteAll("u_id = '$id'");
+    		if ($del and $dels) {
+    			echo "<script>location.href='index.php?r=user/index'</script>";
+    		}else{
+    			echo "<script>alert('失败');location.href='index.php?r=user/index'</script>";
+    		}
     }
 }
