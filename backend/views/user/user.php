ec1230cd81872a6cf993e9be04465eb68db72dc1
zjh <15710071323@163.com>
2016-03-27 20:15:09 +0800

    user

--- backend/views/user/user.php
+++ backend/views/user/user.php
@@ -22,8 +22,45 @@
             <li><a href="index.html">Home</a> <span class="divider">/</span></li>
             <li class="active">Dashboard</li>
         </ul>
+                <div>
+                <center>
+                    
+                    <table border="1" width="800">
+                        <tr>
+                            <td>用户名</td>
+                            <td>昵称</td>
+                            <td>性别</td>
+                            <td>头像</td>
+                            <td>邮箱</td>
+                            <td>电话</td>
+                            <td>积分</td>
+                            <td>创建时间</td>
+                            <td>操作</td>
+                        </tr>
+                        <?php foreach ($select as $key => $value): ?>
+                        <tr>
+                            <td><?php echo $value['u_name']; ?></td>
+                            <td><?php echo $value['m_name']; ?></td>
+                            <td><?php if ($value['m_sex']==1){ ?>
+                                男
+                            <?php }else{ ?>
+                                女
+                            <?php } ?></td>
+                            <td><img src="http://<?php echo $value['m_img']; ?>" alt=""></td>
+                            <td><?php echo $value['m_email']; ?></td>
+                            <td><?php echo $value['m_tel']; ?></td>
+                            <td><?php echo $value['m_integral']; ?></td>
+                            <td><?php echo $value['m_time']; ?></td>
+                            <td><a onclick="if(confirm('确定删除？')) return true; else return false;" href="index.php?r=user/user_del&id=<?php echo $value['u_id']; ?>&type=1" class='outiong'>删除用户</a>
+                            </td>
+                        </tr>
+                        <?php endforeach ?>
+                    </table>
+                </center>
+                </div>
 
-<!--  -->
+
+
                     
                     <footer>
                         <hr>
