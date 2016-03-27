c92acef8a52b89a14c8f379b1140181da6bd079e
zjh <15710071323@163.com>
2016-03-27 20:14:51 +0800

    header

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -132,7 +132,7 @@ AppAsset::register($this);
         <a href="#user-menu" class="nav-header collapsed" data-toggle="collapse"><i class="icon-exclamation-sign"></i>用户管理</a>
         <ul id="user-menu" class="nav nav-list collapse">
             <li ><a href="index.php?r=user/index">用户列表</a></li>
-            <!-- <li ><a href="404.html">404 page</a></li> -->
+            <!-- <li ><a href="404.html">404 page</a>	</li> -->
 <!--             <li ><a href="500.html">500 page</a></li>
             <li ><a href="503.html">503 page</a></li> -->
         </ul>
